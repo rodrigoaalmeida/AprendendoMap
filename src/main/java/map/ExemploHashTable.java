fc62add9f1545cd49b7470eb2b1ad22b2eea8788
rodrigoaalmeida <rodrigo_a.almeida@outlook.com>
2021-03-06 07:44:05 -0400

    Aula HashTable

---
+++ src/main/java/map/ExemploHashTable.java
@@ -0,0 +1,43 @@
+package map;
+
+import java.util.Hashtable;
+import java.util.Map;
+
+public class ExemploHashTable {
+
+    public static void main(String[] args) {
+
+        Hashtable<String, Integer> estudantes = new Hashtable<>();
+
+        estudantes.put("Carlos", 21);
+        estudantes.put("Mariana", 33);
+        estudantes.put("Rafaela", 18);
+        estudantes.put("Pedro", 44);
+
+        System.out.println(estudantes);
+
+        estudantes.put("Pedro", 55);
+
+        System.out.println(estudantes);
+
+        estudantes.remove("Pedro");
+
+        System.out.println(estudantes);
+
+        int idadeEstudante = estudantes.get("Mariana");
+
+        System.out.println(idadeEstudante);
+
+        System.out.println("Tamanho do HashTable: " + estudantes.size());
+
+        for (Map.Entry<String, Integer> entry: estudantes.entrySet()){
+            System.out.println(entry.getKey() + " -- " + entry.getValue());
+        }
+
+        for (String key : estudantes.keySet()){
+            System.out.println(key + " -- " + estudantes.get(key));
+        }
+        
+        System.out.println(estudantes);
+    }
+}
